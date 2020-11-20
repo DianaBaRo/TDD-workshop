c6f42caba71864a8ad99d4f17bea337693c9775a
Peter Dean <peter.dean@smartpension.co.uk>
2020-11-20 11:53:03 +0000

    add test for 0 points at start

--- lib/Game.rb
+++ lib/Game.rb
@@ -5,4 +5,12 @@ class Game
         @player_1 = player_1
         @player_2 = player_2
     end
+
+    def score_P1
+        return 0
+    end
+
+    def score_P2
+        return 0
+    end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Game do
         expect(subject.class). to eq(Game)
         expect(subject.player_1).to eq("A")
     end
+
+   context 'When the game starts' do
+     it 'both players should have 0 points' do
+       expect(subject.score_P1).to eq(0)
+       expect(subject.score_P2).to eq(0)
+     end
+   end
 end
