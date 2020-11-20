e9b13c3117b1e5b22f86b5802fce71c75d0e6048
Diana Badas <diana.badas@smartpension.co.uk>
2020-11-20 12:14:34 +0000

    Add player_wins test and method

--- lib/Game.rb
+++ lib/Game.rb
@@ -1,16 +1,19 @@
 class Game
-    attr_reader :player_1, :player_2
+    attr_reader :player_1, :player_2, :score_P1, :score_P2
 
     def initialize(player_1, player_2)
         @player_1 = player_1
         @player_2 = player_2
+        @score_P1 = 0
+        @score_P2 = 0
     end
 
-    def score_P1
-        return 0
+    def player_wins(player)
+        if player == @player_1
+            @score_P1 += 15
+        else
+            @score_P2 += 15
+        end
     end
 
-    def score_P2
-        return 0
-    end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,15 +2,20 @@ require "./lib/Game.rb"
 
 RSpec.describe Game do
    subject { Game.new("A", "B") }
-    it "returns instance of a game" do
+
+   context 'when we initiaLize a game' do
+     it "returns instance of a game" do
         expect(subject.class). to eq(Game)
         expect(subject.player_1).to eq("A")
-    end
-
-   context 'When the game starts' do
-     it 'both players should have 0 points' do
-       expect(subject.score_P1).to eq(0)
-       expect(subject.score_P2).to eq(0)
+        expect(subject.score_P1).to eq(0)
+        expect(subject.score_P2).to eq(0)
      end
+   end
+
+   context 'when a player wins a point' do
+       it 'reflects in the players points' do
+        subject.player_wins("A")
+           expect(subject.score_P1).to eq(15)
+       end
    end
 end
