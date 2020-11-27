54bece770605bc77bc376c87f4255a0540f93233
Diana Badas <diana.badas@smartpension.co.uk>
2020-11-27 12:06:35 +0000

    Add test when instatiating a player and move test from game_spec to player_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -16,17 +16,6 @@ RSpec.describe Game do
      end
    end
 
-   context 'when a player wins a point' do
-       it 'reflects in the players points' do
-        subject.player_wins(player_1)
-        expect(player_1.score).to eq(15)
-        subject.player_wins(player_1)
-        expect(player_1.score).to eq(30)
-        subject.player_wins(player_1)
-        expect(player_1.score).to eq(40)
-       end
-   end
-
    context 'when we have a winner' do
         it 'will show the results of the game with the name of the winner' do
             player_1.score = 4
---
+++ spec/player_spec.rb
@@ -0,0 +1,16 @@
+require "./lib/player.rb"
+
+RSpec.describe Player do
+    subject { Player.new }
+
+    context 'when we initiaLize a player' do
+        it { is_expected.to have_attributes(score: 0) }
+    end
+
+    context 'when a player wins a point' do
+        it 'reflects in the players points' do
+         subject.wins_point
+         expect(subject.score).to eq(15)
+        end
+    end
+end
